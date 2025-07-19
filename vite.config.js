f6e168211a9cedf7ae5401a4184f8afbda3081ee
rexDeus92 <rashitow.live@gmail.com>
2025-07-19 06:50:55 +0500

    second build-vite-config

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/Copy-SoundCloud/', // имя твоего репозитория!
+  base: '/Copy-SoundCloud/', // имя твоего репозитория!
 });
